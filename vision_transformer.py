87a4799b5e90efc4528cd809c3aa897bc7fb3498
seonghapark <park708@purdue.edu>
2022-09-15 16:14:47 -0500

    backup

--- vision_transformer.py
+++ vision_transformer.py
@@ -1,11 +1,11 @@
 # Copyright (c) Facebook, Inc. and its affiliates.
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -21,7 +21,7 @@ from functools import partial
 import torch
 import torch.nn as nn
 
-from utils import trunc_normal_
+from vit_utils import trunc_normal_
 
 
 def drop_path(x, drop_prob: float = 0., training: bool = False):
